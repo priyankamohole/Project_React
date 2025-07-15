529b19be85a1f67380bbd746fc25e742a3ce51ec
priyankamohole <priyankamohole@gmail.com>
2025-07-15 10:55:28 +0530

    update

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { ThemeProvider } from './components/Theme'
 import './App.css'
+import ThemeBtn from './components/Themebtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  const darkTheme = () => setThemeMode("dark")
+  const lightTheme = () => setThemeMode("light")
+
+  //actual theme switcher
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light","dark")
+    document.querySelector("html").classList.add(themeMode)
+  }, [themeMode])
+
+  return ( 
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                {/*theme btn */}
+                <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+                {/*card */}
+                <Card/>
+            </div>
+        </div>
+    </div>
+ </ThemeProvider>
+    )
+  }
+
 
 export default App
